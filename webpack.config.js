56174ab8c0d524935d2b16f0dbe4ace8a500fc93
Elif Sezgin <elifmsezgin@gmail.com>
2017-02-08 01:28:29 -0800

    Made setup, configurations, drag-drop dna object

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  context: __dirname,
+  entry: "./lib/entry.js",
+  output: {
+    path: './',
+    filename: 'bundle.js',
+  },
+  module: {
+    loaders: [
+      {
+        test: [/\.jsx?$/, /\.js?$/],
+        exclude: /(node_modules)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015']
+        }
+      }
+    ]
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions: ['.js', '.jsx']
+  }
+};
