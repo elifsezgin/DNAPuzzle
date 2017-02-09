35642b8dfc9e60a4c69290cb214637d0ac6b0ca5
Elif Sezgin <elifmsezgin@gmail.com>
2017-02-09 13:10:51 -0800

    Refactored game.js

--- assets/bundle.js
+++ assets/bundle.js
@@ -364,6 +364,8 @@ var Game = function () {
   _createClass(Game, [{
     key: 'createDNAs',
     value: function createDNAs() {
+      var _this = this;
+
       var count = 3 + Math.floor(Math.random() * 4);
       var that = this;
 
@@ -380,37 +382,25 @@ var Game = function () {
         var dnaY = dna.dnaY;
         var strandX = dna.strandX;
         var strandY = dna.strandY;
-        var paired = false;
-        // debugger;
 
         dna.dnaCanvas.addEventListener("mousedown", function (evt) {
           dna.dnaCanvas.localX = evt.localX;
           dna.dnaCanvas.localY = evt.localY;
-          if (paired) {
-
-            dna.strandCanvas.localX = evt.localX;
-            dna.strandCanvas.localY = evt.localY;
-          }
         });
         dna.strandCanvas.addEventListener("mousedown", function (evt) {
-          if (!paired) {
-            dna.strandCanvas.localX = evt.localX;
-            dna.strandCanvas.localY = evt.localY;
-          }
+          dna.strandCanvas.localX = evt.localX;
+          dna.strandCanvas.localY = evt.localY;
         });
         dna.strandCanvas.addEventListener("pressup", function (evt) {
           dna.strandCanvas.localX = evt.localX;
           dna.strandCanvas.localY = evt.localY;
+          dna.strandX = strandX + dna.strandCanvas.x;
+          dna.strandY = strandY + dna.strandCanvas.y;
           var distance = (dna.strandX - dna.gapX) * (dna.strandX - dna.gapX) + (dna.strandY - dna.gapY) * (dna.strandY - dna.gapY);
           if (distance < 100) {
-            console.log('on the DNA');
-            paired = true;
             dna.pair(dna.dnaCanvas, dna.strandCanvas);
             that.container.removeChild(dna.strandCanvas);
-            that.stage.update();
-            // for (let i = 0; i < length; i++) {
-            //   sequence.push(BASES[Math.floor(Math.random() * 4)]);
-            // }
+            _this.stage.update();
           }
         });
         dna.dnaCanvas.on("pressmove", function (evt) {
@@ -420,30 +410,11 @@ var Game = function () {
           dna.gapY = gapY + dna.dnaCanvas.y;
           dna.dnaX = dnaX + dna.dnaCanvas.x;
           dna.dnaY = dnaY + dna.dnaCanvas.y;
-          // dna.dnaCanvas.graphics.beginStroke('#ff7f41').beginFill('#ff7f41').drawCircle(dna.gapX, dna.gapY, 5)
-          // dna.dnaCanvas.graphics.beginStroke('#4162ff').beginFill('#4162ff').drawCircle(dna.dnaX, dna.dnaY, 5)
-          if (paired) {
-            dna.strandCanvas.x = dna.dnaCanvas.x;
-            dna.strandCanvas.y = dna.dnaCanvas.x;
-            dna.strandX = strandX + dna.strandCanvas.x;
-            dna.strandY = strandY + dna.strandCanvas.y;
-          }
-
           that.stage.update();
         });
         dna.strandCanvas.on("pressmove", function (evt) {
-          if (!paired) {
-            dna.strandCanvas.x = evt.stageX - dna.strandCanvas.localX;
-            dna.strandCanvas.y = evt.stageY - dna.strandCanvas.localY;
-            dna.strandX = strandX + dna.strandCanvas.x;
-            dna.strandY = strandY + dna.strandCanvas.y;
-            // dna.strandCanvas.graphics.beginStroke('#4162ff').beginFill('#4162ff').drawCircle(dna.strandX, dna.strandY, 5)
-            // Game logic here
-            var distance = (dna.strandX - dna.gapX) * (dna.strandX - dna.gapX) + (dna.strandY - dna.gapY) * (dna.strandY - dna.gapY);
-            if (distance < 100) {
-              console.log('close to the DNA');
-            }
-          }
+          dna.strandCanvas.x = evt.stageX - dna.strandCanvas.localX;
+          dna.strandCanvas.y = evt.stageY - dna.strandCanvas.localY;
           that.stage.update();
         });
         that.stage.update();
